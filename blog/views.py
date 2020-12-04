7f56c4fbe6ff7b62ed881c36248d67d9a61c88b8
praveen536 <praveensahu536@gmail.com>
2020-12-04 18:55:01 +0530

    blog prject

---
+++ blog/views.py
@@ -0,0 +1,115 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.utils import timezone
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import AuthenticationForm
+# Create your views here.
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin ,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class PostUpdateView(LoginRequiredMixin ,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model=Post    
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        # return Post.objects.all()
+        return Post.objects.filter(publish_date__isnull=True).order_by('create_date')
+
+
+
+#################################################
+#################################################
+@login_required
+def post_publish(request,pk):
+    # post = get_object_or_404(Post,pk=pk)
+    # post.publish()
+    # return redirect('post_detail',pk=pk)
+    Post.objects.filter(pk=pk).update(publish_date=timezone.now())
+    return redirect('post_list')
+
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method=='POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html', {'form':form})
+
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk=comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk )
+
+
+class LoginView(TemplateView):
+    template_name = 'registration/login.html'
+
+
+@login_required
+def LogoutView(TemplateView):
+    template_name = 'registration/login.html'
+
+
+
+from django.contrib.auth import authenticate, login
+
+def my_login(request):
+    username = request.POST['username']
+    password = request.POST['passowrd']
+
+    user = authenticate(request, username=username, pasword=password)
+    if user is not None:
+        login(request, user)
+        return redirect('post_list')
+    else:
+        return redirect('post_list')
